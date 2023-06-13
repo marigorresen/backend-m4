9ee4369578724a74b7409c2f8bb2bb11333405b7
Mariana Görresen <110499531+marigorresen@users.noreply.github.com>
2023-06-13 23:34:04 +0000

    Update index.js and pirelli.db

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const path = require("path");
 
@@ -7,18 +7,18 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader("Acess-Control-Allow-Origin", "*");
-    next();
+  res.setHeader("Acess-Control-Allow-Origin", "*");
+  next();
 });
 
-const usersRoutes = require('./routes/user.js');
-const funcRoutes = require('./routes/funcionarios.js');
-const histRoutes = require('./routes/historico.js');
-const dispoRoutes = require('./routes/dispositivos.js');
+const usersRoutes = require("./routes/user.js");
+const funcRoutes = require("./routes/funcionarios.js");
+const histRoutes = require("./routes/historico.js");
+const dispoRoutes = require("./routes/dispositivos.js");
 
-app.use('/user', usersRoutes);
-app.use('/func', funcRoutes);
-app.use('/hist', histRoutes);
-app.use('/dispo', dispoRoutes);
+app.use("/user", usersRoutes);
+app.use("/func", funcRoutes);
+app.use("/hist", histRoutes);
+app.use("/dispo", dispoRoutes);
 
-app.listen(3000, () => console.log('Listening at port 3000'));
+app.listen(3000, () => console.log("Listening at port 3000"));
