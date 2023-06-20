64a949d1f2f8e3f601e1f6e12d10a7a518528b42
Mariana Görresen <110499531+marigorresen@users.noreply.github.com>
2023-06-20 14:17:34 +0000

    Update 7 files

--- routes/funcionarios.js
+++ routes/funcionarios.js
@@ -1,17 +1,73 @@
-const express = require('express');
-const sqlite3 = require('sqlite3');
+const express = require("express");
+const sqlite3 = require("sqlite3");
 
 const router = express.Router();
-const db = new sqlite3.Database('pirelli.db');
+const db = new sqlite3.Database("pirelli.db");
 
-// Rota GET para obter todos os usuários
-router.get('/', (req, res) => {
-  const query = 'SELECT * FROM funcionarios';
+// Rota GET para obter todos os funcionarios
+router.get("/", (req, res) => {
+  const query = "SELECT * FROM funcionarios";
 
   db.all(query, (err, rows) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Error fetching funcionarios' });
+      return res.status(500).json({ error: "Error fetching funcionarios" });
+    }
+
+    res.json(rows);
+  });
+});
+
+// Rota GET para obter id_empresa dos funcionarios
+router.get("/id_empresa", (req, res) => {
+  const query = "SELECT id_empresa FROM funcionarios";
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error fetching funcionarios" });
+    }
+
+    res.json(rows);
+  });
+});
+
+// Rota GET para obter nomes dos funcionarios
+router.get("/nome", (req, res) => {
+  const query = "SELECT nome FROM funcionarios";
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error fetching funcionarios" });
+    }
+
+    res.json(rows);
+  });
+});
+
+// Rota GET para obter emails dos funcionarios
+router.get("/email", (req, res) => {
+  const query = "SELECT email FROM funcionarios";
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error fetching funcionarios" });
+    }
+
+    res.json(rows);
+  });
+});
+
+// Rota GET para obter telefones dos funcionarios
+router.get("/telefone", (req, res) => {
+  const query = "SELECT telefone FROM funcionarios";
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error fetching funcionarios" });
     }
 
     res.json(rows);
@@ -19,18 +75,18 @@ router.get('/', (req, res) => {
 });
 
 // Rota GET para obter um usuário específico
-router.get('/:id', (req, res) => {
-  const query = 'SELECT * FROM funcionarios WHERE id = ?';
+router.get("/:id", (req, res) => {
+  const query = "SELECT * FROM funcionarios WHERE id = ?";
   const userId = req.params.id;
 
   db.get(query, [userId], (err, row) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Error fetching func' });
+      return res.status(500).json({ error: "Error fetching func" });
     }
 
     if (!row) {
-      return res.status(404).json({ error: 'Func not found' });
+      return res.status(404).json({ error: "Func not found" });
     }
 
     res.json(row);
@@ -38,16 +94,17 @@ router.get('/:id', (req, res) => {
 });
 
 // Rota POST para criar um novo usuário
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const { nome, id_empresa, email, telefone } = req.body;
 
-  const query = 'INSERT INTO funcionarios (nome, id_empresa, email, telefone) VALUES (?, ?, ?, ?)';
+  const query =
+    "INSERT INTO funcionarios (nome, id_empresa, email, telefone) VALUES (?, ?, ?, ?)";
   const values = [nome, id_empresa, email, telefone];
 
   db.run(query, values, function (err) {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Error creating func' });
+      return res.status(500).json({ error: "Error creating func" });
     }
 
     res.json({ id: this.lastID, nome, id_empresa, email, telefone });
@@ -55,21 +112,22 @@ router.post('/', (req, res) => {
 });
 
 // Rota PUT para atualizar um usuário existente
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   const { nome, id_empresa, email, telefone } = req.body;
   const userId = req.params.id;
 
-  const query = 'UPDATE funcionarios SET nome = ?, id_empresa = ?, email = ?, telefone = ? WHERE id = ?';
+  const query =
+    "UPDATE funcionarios SET nome = ?, id_empresa = ?, email = ?, telefone = ? WHERE id = ?";
   const values = [nome, id_empresa, email, telefone, userId];
 
   db.run(query, values, function (err) {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Error updating func' });
+      return res.status(500).json({ error: "Error updating func" });
     }
 
     if (this.changes === 0) {
-      return res.status(404).json({ error: 'Func not found' });
+      return res.status(404).json({ error: "Func not found" });
     }
 
     res.json({ id: userId, nome, id_empresa, email, telefone });
@@ -77,19 +135,19 @@ router.put('/:id', (req, res) => {
 });
 
 // Rota DELETE para excluir um usuário
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const userId = req.params.id;
 
-  const query = 'DELETE FROM funcionarios WHERE id = ?';
+  const query = "DELETE FROM funcionarios WHERE id = ?";
 
   db.run(query, [userId], function (err) {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Error deleting func' });
+      return res.status(500).json({ error: "Error deleting func" });
     }
 
     if (this.changes === 0) {
-      return res.status(404).json({ error: 'Func not found' });
+      return res.status(404).json({ error: "Func not found" });
     }
 
     res.sendStatus(204);
